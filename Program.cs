d9341d581cc014b2ec98b7ab7465b0b4651a0e5b
瑋芝 張 <juice121457@gmail.com>
2021-03-12 09:39:10 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("----啟動程式----");
+            }
+
+            string method = args[0];
+            string path = args[1];
+            
+
+            //ReadFile時用這個↓↓↓
+            if(method.ToLower() == "readfile")
+            {
+                Class1.ReadFile(path);
+            }
+            //ReadFile時用這個↑↑↑
+
+
+
+            if(method.ToLower() == "movefile")
+            {
+                string path2 = args[2];
+                Class1.MoveFile(path, path2);
+            }
+
+            Console.WriteLine("----程式結束----");
+
+
+            Console.ReadLine();
+        }
+
+        
+        
+    }
+}
---
+++ read.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleApp6
+{
+    class Class1
+    {
+        public static void ReadFile(string path)
+        {
+            if (!File.Exists(path))  //判斷檔案是否存在，如果不存在則結束程式
+            {
+                Console.WriteLine("找不到檔案");
+                return;
+            }
+
+            Console.Write("是否繼續 ? (Y/N):");
+            string yes = Console.ReadLine().ToLower(); //將使用者輸入轉乘小寫
+
+
+            if (yes == "yes" || yes == "y")  
+            {
+                
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();  //開始計時
+
+                string readFile = File.ReadAllText(path);  //讀取檔案裡的文字
+
+                
+                Console.WriteLine(readFile);
+                stopWatch.Stop(); //結束計時
+
+                TimeSpan ts = stopWatch.Elapsed;
+                Console.WriteLine($"檔案讀取完成,共使用{ts.TotalMilliseconds}毫秒");
+
+
+            }
+            else
+            {
+                return;
+            }
+            
+
+            
+        }
+
+        public static void MoveFile(string fromPath, string toPaht)
+        {
+
+        }
+
+    }
+}
